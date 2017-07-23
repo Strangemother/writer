de4a866b16b3c853611d0476619b0704f0e19f79
Jay Jagpal <jay@strangemother.com>
2017-07-23 16:39:12 +0100

    Text session refactor

--- writer/writer/static/js/mdwriter/workers/session-worker.js
+++ writer/writer/static/js/mdwriter/workers/session-worker.js
@@ -2,7 +2,60 @@
 manage the data session of a page, tracking character entry and page context
  */
 
-class TextSessionWorker extends ManagerComponent {
+class BlockManager extends ManagerComponent {
+
+    replaceBlock(blockIndex, newBlock) {
+        console.log('replacing block', blockIndex)
+        this.insertBlockAt(blockIndex+1, newBlock)
+        return this.removeBlockAt(blockIndex)
+    }
+
+    splitBlockAtIndex(blockIndex, strIndex) {
+        /* split the block into two blocks, the second block indexed under the blockIndex*/
+        let text = this.session.blocks[blockIndex].text;
+        this.updateBlockText(blockIndex, text.slice(0, strIndex))
+        return this.insertBlockAt(blockIndex + 1, this.asBlock(text.slice(strIndex)))
+    }
+
+    asBlock(text) {
+        return {
+            text: text
+        }
+    }
+
+    asBlocks(texts) {
+        let r = []
+        for (var i = 0; i < texts.length; i++) {
+            r.push(this.asBlock(texts[i]))
+        };
+        return r;
+    }
+
+    updateBlockText(index, text, meta){
+        /* update the block of given index with the new text.*/
+        this.session.blocks[index].text = text;
+    }
+
+    insertBlockAt(index, block) {
+        /* Push a single block into the given index position */
+        return this.insertBlocksAt(index, [block])
+    }
+
+    removeBlockAt(index) {
+        return this.session.blocks.splice(1, 1)
+    }
+
+    insertBlocksAt(index, blocks) {
+        /* insert many blocks into the block list
+        returns the new blocks length*/
+        console.log('block insert at:', index, 'count:', blocks.length)
+        this.session.blocks.splice(index, 0, ...blocks)
+        return this.session.blocks.length
+    }
+}
+
+
+class TextSessionWorker extends BlockManager {
 
     init(){
         this.session = {
@@ -41,6 +94,8 @@ class TextSessionWorker extends ManagerComponent {
         } else {
             this.insertLines(e)
         }
+
+        console.log('Line count:', this.session.blocks.length)
     }
 
     insertLine(e){
@@ -51,10 +106,89 @@ class TextSessionWorker extends ManagerComponent {
         */
         let blockIndex = e.start.row
             , sCol = e.start.column
-            , eCol = e.end.column
+            //, eCol = e.end.column
             , value = e.lines[0]
 
         this.injectTextInBlock(blockIndex, sCol, value)
+    }
+
+    _firstlineInsert(e) {
+
+        let end = '';
+
+        if(e.start.column == 0) {
+            // no need to slice the string; add a line before.
+            this.replaceBlock(e.start.row, this.asBlock(e.lines[0]))
+        } else {
+
+            let text = this.session.blocks[e.start.row].text
+                , strIndex = e.start.column
+                // slice the string at the index of of start/end.
+                , start = text.slice(0, strIndex)
+                // this is faster than an array join
+                , newText = text.substr(0, strIndex) + e.lines[0]
+                ;
+            end = text.slice(strIndex)
+            this.updateBlockText(e.start.row, newText)
+        }
+
+        return end;
+    }
+
+    insertLines(e){
+        /* inset multiple lines into a the existing blocks. Additional blocks
+        will be created.
+        */
+
+        if(e.lines.join('').length == 0) {
+            return this.insertReturn(e)
+        }
+
+        // the first line
+        let end = this._firstlineInsert(e);
+
+        // skip first line, inject new lines
+        let midLines = this.asBlocks(e.lines.slice(1, -1));
+        // generate the last line
+        midLines = midLines.concat(this.asBlock(e.lines[e.lines.length-1] + end))
+
+        let [isMatch, endRows, dupIndex] = this.detectDuplicates(midLines, e.start.row)
+
+        if(isMatch) {
+            /* detected the same lines provided to the API*/
+            console.log('discovered', endRows.length, 'duplicated')
+            this.insertBlocksAt(e.end.row + dupIndex + 1, endRows)
+            return
+        }
+
+        // debugger;
+        console.log('Applying', midLines.length ,'new rows')
+        this.insertBlocksAt(e.start.row+1, midLines)
+
+        return;
+
+        // for (var i = 1; i < e.lines.length; i++) {
+        //     let line = e.lines[i];
+        //     let block = this.session.blocks[i];
+        //     if(block == undefined) {
+        //         console.log('Detect append to end')
+        //         this.session.blocks.push({ text: line })
+        //     } else if(e.start.column == 0
+        //         && line == block.text) {
+        //         console.log('detect line replacement.')
+        //     } else {
+        //         if(i == 0){
+        //             continue
+        //         } else if(i == e.lines.length - 1) {
+        //             // add a new block as a merge of last line and end line.
+        //             line = line + end;
+        //         }
+
+        //         this.insertBlockAt(i+1, this.asBlock(line))
+
+        //     }
+
+        // };
     }
 
     injectTextInBlock(blockIndex, strIndex, value) {
@@ -71,45 +205,29 @@ class TextSessionWorker extends ManagerComponent {
             ;
 
         console.log('add text to block', blockIndex)
-        this.updateBlock(blockIndex, newText)
+        this.updateBlockText(blockIndex, newText)
     }
 
+    detectDuplicates(lines, offsetIndex=0) {
 
-    updateBlock(index, text, meta){
-        /* update the block of given index with the new text.*/
-        this.session.blocks[index].text = text;
-    }
-
-    insertLines(e){
-        /* inset multiple lines into a the existing blocks. Additional blocks
-        will be created.
-        */
-
-        if(e.lines.join('').length == 0) {
-            return this.insertReturn(e)
-        }
-
-        console.log('insertLines')
-
-        // skip first line, inject new lines
-        for (var i = 0; i < e.lines.length; i++) {
-            let line = e.lines[i];
-            let block = this.session.blocks[i];
-            if(block == undefined) {
-                console.log('Detect append to end')
-                this.session.blocks.push({ text: line })
-            } else if(e.start.column == 0
-                && line == block.text) {
-                console.log('detect line replacement.')
-            } else {
-                debugger
-                // this.injectTextInBlock(i, e.start.column, line)
+        var isMatch = true;
+        let endRows = [];
+        let dupIndex = 0;
+        for (var i = 0; i < lines.length; i++) {
+            if(this.session.blocks[offsetIndex+i+1] == undefined) {
+                endRows = lines.slice(i)
+                dupIndex = i;
+                continue
             }
 
+            if (this.session.blocks[offsetIndex+i+1].text != lines[i].text)  {
+                isMatch = false;
+                console.log('Dup detection stop.')
+                break;
+            }
         };
 
-        debugger
-
+        return [isMatch, endRows, dupIndex];
     }
 
     insertReturn(e) {
@@ -119,34 +237,10 @@ class TextSessionWorker extends ManagerComponent {
         console.log('inserted enter key', c)
     }
 
-    splitBlockAtIndex(blockIndex, strIndex) {
-        /* split the block into two blocks, the second block indexed under the blockIndex*/
-        let text = this.session.blocks[blockIndex].text;
-        let startL = text.slice(0, strIndex)
-        let endL = text.slice(strIndex)
-        this.updateBlock(blockIndex, startL)
-        return this.insertBlockAt(blockIndex + 1, this.asBlock(endL))
-    }
-
-    asBlock(text) {
-        return {
-            text: text
-        }
-    }
-
-    insertBlockAt(index, block) {
-        /* Push a single block into the given index position */
-        return this.insertBlocksAt(index, [block])
-    }
-
-    insertBlocksAt(index, blocks) {
-        /* insert many blocks into the block list
-        returns the new blocks length*/
-        console.log('block insert at:', index, blocks.length)
-        this.session.blocks.splice(index, 0, ...blocks)
-        return this.session.blocks.length
-    }
 }
 
 
 TextSessionWorker.mount()
+
+
+
