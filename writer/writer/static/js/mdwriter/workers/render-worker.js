d7d86cad66d692ff5da27809282a9d42da2de8a2
Jay Jagpal <jay@strangemother.com>
2017-07-23 02:41:49 +0100

    markdown renderer

--- writer/writer/static/js/mdwriter/workers/render-worker.js
+++ writer/writer/static/js/mdwriter/workers/render-worker.js
@@ -1,9 +1,55 @@
 //console.log('new render-worker', ManagerComponent)
+importScripts(
+    '/static/js/marked/lib/marked.js'
+    , '/static/js/vendor/highlight.pack.js'
+)
+
+
+class MarkdownRenderer {
+    constructor() {
+        marked.setOptions({
+          renderer: new marked.Renderer(),
+          gfm: true,
+          tables: true,
+          breaks: false,
+          pedantic: false,
+          sanitize: false,
+          smartLists: true,
+          smartypants: false,
+          highlight: function (code) {
+            return hljs.highlightAuto(code).value;
+          }
+        });
+    }
+
+    renderLines(lines){
+        return this.render(lines.join('\n'))
+    }
+
+    render(s){
+        var self = this;
+        return marked(s, (function(data){
+            return function (err, rs) {
+                if (err) throw err;
+                self.rendered(rs,s);
+            }
+       })(data) ) ;
+    }
+
+    rendered(rStr, oStr) {
+        postMessage({
+            type: 'render'
+            , content: rStr
+        })
+    }
+}
+
 
 class RenderWorker extends ManagerComponent {
 
     init(){
-        console.log('new render-worker')
+        this.renderer = new MarkdownRenderer();
+
     }
 
     mountMethods() {
@@ -12,7 +58,7 @@ class RenderWorker extends ManagerComponent {
         }
     }
 
-    receiveEvent(e) {
+    _receiveEvent(e) {
         if( e.action != undefined
             && this[`${e.action}ActionEvent`] != undefined) {
             return [true, this[`${e.action}ActionEvent`](e)]
@@ -22,12 +68,14 @@ class RenderWorker extends ManagerComponent {
     }
 
     insertActionEvent(e) {
-        console.log('action event')
+        console.log('RenderWorker insert action')
+        return this.render(e)
     }
 
     render(t) {
-        console.log('render the given text.')
+        return 'doggy'
     }
 }
+
 
 RenderWorker.mount()
